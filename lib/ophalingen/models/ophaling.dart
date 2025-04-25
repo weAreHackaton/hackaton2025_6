98369a816cb7ccda154c44738170dd16bc7b37dc
Kaya-Sem <kayasemvc@gmail.com>
2025-04-25 20:53:35 +0200

    new foodtypes

--- lib/ophalingen/models/ophaling.dart
+++ lib/ophalingen/models/ophaling.dart
@@ -1,11 +1,24 @@
 import 'package:equatable/equatable.dart';
 import 'package:hackaton2025_6/package.dart';
 
+enum FoodType {
+  vegetables,
+  fruits,
+  meat,
+  fish,
+  dairy,
+  bread,
+  pastries,
+  preparedFood,
+  other,
+}
+
 class Ophaling extends Equatable {
   final User user;
+  final List<FoodType> foodtypes;
 
-  const Ophaling({required this.user});
+  const Ophaling({required this.user, required this.foodtypes});
 
   @override
-  List<Object?> get props => [user];
+  List<Object?> get props => [user, foodtypes];
 }
