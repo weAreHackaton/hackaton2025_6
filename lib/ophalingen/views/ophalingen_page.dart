93f9c1865dc84e2599cae266d65bf1aa9332db12
noahvsb <noahvansteenbrugge@gmail.com>
2025-04-26 12:03:27 +0200

    import fixes

--- lib/ophalingen/views/ophalingen_page.dart
+++ lib/ophalingen/views/ophalingen_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hackaton2025_6/package.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
-import 'info_card.dart';
 
 class OphalingenPage extends StatelessWidget {
   const OphalingenPage({super.key});
