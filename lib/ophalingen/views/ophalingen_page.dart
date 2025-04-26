3f64ab305e1bb0213a4905fe5f233e236858e227
noahvsb <noahvansteenbrugge@gmail.com>
2025-04-26 12:55:39 +0200

    refresh indicator

--- lib/ophalingen/views/ophalingen_page.dart
+++ lib/ophalingen/views/ophalingen_page.dart
@@ -2,35 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:hackaton2025_6/package.dart';
 import 'package:provider/provider.dart';
 
-class OphalingenPage extends StatelessWidget {
+class OphalingenPage extends StatefulWidget {
   const OphalingenPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    List<Ophaling> ophalingen = context.read<OphalingRepository>().getTempOphalingen();
+  State<OphalingenPage> createState() => _OphalingenPageState();
+}
 
+class _OphalingenPageState extends State<OphalingenPage> {
+  late List<Ophaling> _ophalingen;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadOphalingen();
+  }
+
+  void _loadOphalingen() {
+    final ophalingen = context.read<OphalingRepository>().getTempOphalingen();
+    setState(() {
+      _ophalingen = ophalingen;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Beschikbare ophalingen'),
       ),
-      body: ListView.builder(
-        itemCount: ophalingen.length,
-        itemBuilder: (context, index) {
-          final ophaling = ophalingen[index];
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailPage(ophaling: ophaling),
-                ),
-              );
-            },
-            child: info_card(
-              ophaling: ophaling,
-            ),
-          );
+      body: RefreshIndicator(
+        onRefresh: () async {
+          _loadOphalingen();
         },
+        child: ListView.builder(
+          itemCount: _ophalingen.length,
+          itemBuilder: (context, index) {
+            final ophaling = _ophalingen[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailPage(ophaling: ophaling),
+                  ),
+                );
+              },
+              child: info_card(
+                ophaling: ophaling,
+              ),
+            );
+          },
+        ),
       ),
     );
   }
