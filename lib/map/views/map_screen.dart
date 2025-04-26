04d12a54a95d94751a22010993bb48814198b3e1
Kaya-Sem <kayasemvc@gmail.com>
2025-04-26 09:18:35 +0200

    refactored ophaling

--- lib/map/views/map_screen.dart
+++ lib/map/views/map_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:hackaton2025_6/ophalingen/models/ophaling.dart';
+import 'package:hackaton2025_6/ophalingen/data/sample_ophalingen.dart';
 import 'package:hackaton2025_6/ophalingen/views/detail_page.dart';
 import 'package:hackaton2025_6/profiel/models/user.dart';
 import 'package:intl/intl.dart';
@@ -17,61 +17,6 @@ class _MapScreenState extends State<MapScreen> {
   final MapController _mapController = MapController();
   final List<Marker> _markers = [];
 
-  // Hardcoded Ophaling objects
-  final List<Ophaling> _ophalingen = [
-    Ophaling(
-      user: User(
-        name: 'John Doe',
-        email: 'john@example.com',
-        phoneNumber: '+32 123 45 67 89',
-        location: LatLng(51.0543, 3.7174),
-      ),
-      foodtypes: [FoodType.vegetables, FoodType.fruits],
-      start: DateTime.now().add(const Duration(hours: 2)),
-      end: DateTime.now().add(const Duration(hours: 4)),
-      transportType: TransportType.cargoBike,
-      needsRefrigeration: false,
-      description: 'Verse groenten en fruit van de lokale markt',
-      location: LatLng(51.2194, 4.4025),
-      maxVolunteers: 2,
-      currentVolunteers: 1,
-    ),
-    Ophaling(
-      user: User(
-        name: 'Jane Smith',
-        email: 'jane@example.com',
-        phoneNumber: '+32 123 45 67 89',
-        location: LatLng(51.0543, 3.7174),
-      ),
-      foodtypes: [FoodType.bread, FoodType.pastries],
-      start: DateTime.now().add(const Duration(hours: 5)),
-      end: DateTime.now().add(const Duration(hours: 7)),
-      transportType: TransportType.minivan,
-      needsRefrigeration: false,
-      description: 'Koffiekoeken van de bakker om de hoek',
-      location: LatLng(50.8503, 4.3517),
-      maxVolunteers: 1,
-      currentVolunteers: 0,
-    ),
-    Ophaling(
-      user: User(
-        name: 'Mike Johnson',
-        email: 'mike@example.com',
-        phoneNumber: '+32 123 45 67 89',
-        location: LatLng(51.0543, 3.7174),
-      ),
-      foodtypes: [FoodType.meat, FoodType.fish],
-      start: DateTime.now().add(const Duration(hours: 8)),
-      end: DateTime.now().add(const Duration(hours: 10)),
-      transportType: TransportType.truck,
-      needsRefrigeration: true,
-      description: 'Vers vlees en vis uit de carrefour',
-      location: LatLng(51.0543, 3.7174),
-      maxVolunteers: 3,
-      currentVolunteers: 2,
-    ),
-  ];
-
   @override
   void initState() {
     super.initState();
@@ -79,8 +24,8 @@ class _MapScreenState extends State<MapScreen> {
   }
 
   void _addMarkers() {
-    for (int i = 0; i < _ophalingen.length; i++) {
-      final ophaling = _ophalingen[i];
+    for (int i = 0; i < SampleOphalingen.ophalingen.length; i++) {
+      final ophaling = SampleOphalingen.ophalingen[i];
       
       _markers.add(
         Marker(
@@ -176,7 +121,7 @@ class _MapScreenState extends State<MapScreen> {
       body: FlutterMap(
         mapController: _mapController,
         options: MapOptions(
-          initialCenter: _ophalingen[0].location,
+          initialCenter: SampleOphalingen.ophalingen[0].location,
           initialZoom: 8.0,
           onTap: (_, __) {},
           interactionOptions: const InteractionOptions(
--- lib/ophalingen/views/ophalingen_page.dart
+++ lib/ophalingen/views/ophalingen_page.dart
@@ -1,86 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:hackaton2025_6/package.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:hackaton2025_6/ophalingen/data/sample_ophalingen.dart';
+import 'package:hackaton2025_6/ophalingen/views/detail_page.dart';
 import 'package:intl/intl.dart';
-
-import 'detail_page.dart';
-import 'InfoCard.dart';
 
 class OphalingenPage extends StatelessWidget {
   const OphalingenPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // List of business/store names
-    final List<String> businessNames = [
-      'Bakker X',
-      'Aldi',
-      'Carrefour',
-      'Lidl',
-      'Delhaize',
-      'Colruyt',
-      'Jumbo',
-      'Albert Heijn',
-      'Spar',
-      'Coop',
-      'Bakkerij De Smet',
-      'Slagerij Van Damme',
-      'Viswinkel De Zee',
-      'Groenteboer Van Dijk',
-      'Koffiebar Java',
-      'Restaurant De Keuken',
-      'Cafetaria De Friet',
-      'Patisserie Sweet',
-      'Supermarkt Plus',
-      'Marktkraam Groenten'
-    ];
-
-    // Generate sample data
-    final List<Ophaling> ophalingen = List.generate(
-      20,
-      (index) => Ophaling(
-         user: User(
-          name: businessNames[index],
-          email: 'contact@${businessNames[index].toLowerCase().replaceAll(' ', '')}.be',
-          phoneNumber: '+32 123 45 67 89',
-          location: LatLng(51.0543, 3.7174),
-        ),
-        foodtypes: [
-          FoodType.values[index % FoodType.values.length],
-          FoodType.values[(index + 1) % FoodType.values.length],
-        ],
-        start: DateTime.now().add(Duration(hours: index)),
-        end: DateTime.now().add(Duration(hours: index + 2)),
-        description: 'Food collection from ${businessNames[index]}',
-        location: LatLng(51.0543 + (index * 0.01), 3.7174 + (index * 0.01)),
-        transportType: TransportType.values[index % TransportType.values.length],
-        needsRefrigeration: index % 3 == 0,
-        maxVolunteers: (index % 3) + 1,
-        currentVolunteers: index % 2,
-      ),
-    );
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Ophalingen'),
       ),
       body: ListView.builder(
-        itemCount: ophalingen.length,
+        itemCount: SampleOphalingen.ophalingen.length,
         itemBuilder: (context, index) {
-          final ophaling = ophalingen[index];
-          return GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => DetailPage(ophaling: ophaling),
-                ),
-              );
-            },
-            child: InfoCard(
-              address: ophaling.user.name,
-              description: '${ophaling.description}\nVrijwilligers: ${ophaling.currentVolunteers}/${ophaling.maxVolunteers}',
-              tags: ophaling.foodtypes,
+          final ophaling = SampleOphalingen.ophalingen[index];
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            child: ListTile(
+              title: Text(ophaling.user.name),
+              subtitle: Text(
+                '${DateFormat('HH:mm').format(ophaling.start)} - ${DateFormat('HH:mm').format(ophaling.end)}',
+              ),
+              trailing: Text('${ophaling.currentVolunteers}/${ophaling.maxVolunteers}'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => DetailPage(ophaling: ophaling),
+                  ),
+                );
+              },
             ),
           );
         },
