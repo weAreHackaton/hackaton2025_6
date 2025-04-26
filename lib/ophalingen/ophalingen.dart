1beabb05a0e199d4f3105342ff4b1c2edaa6eb74
noahvsb <noahvansteenbrugge@gmail.com>
2025-04-26 12:01:53 +0200

    OphalingenProvider provides SampleOphalingen

--- lib/ophalingen/ophalingen.dart
+++ lib/ophalingen/ophalingen.dart
@@ -2,3 +2,4 @@ export 'views/views.dart';
 export 'models/ophaling.dart';
 export 'data/sample_ophalingen.dart';
 export 'widgets/widgets.dart';
+export 'repository/ophaling_repository.dart';
--- lib/ophalingen/views/ophalingen_page.dart
+++ lib/ophalingen/views/ophalingen_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:hackaton2025_6/ophalingen/data/sample_ophalingen.dart';
-import 'package:hackaton2025_6/ophalingen/views/detail_page.dart';
+import 'package:hackaton2025_6/package.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 import 'info_card.dart';
 
 class OphalingenPage extends StatelessWidget {
@@ -9,14 +9,16 @@ class OphalingenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Ophaling> ophalingen = context.read<OphalingRepository>().getTempOphalingen();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Beschikbare ophalingen'),
       ),
       body: ListView.builder(
-        itemCount: SampleOphalingen.ophalingen.length,
+        itemCount: ophalingen.length,
         itemBuilder: (context, index) {
-          final ophaling = SampleOphalingen.ophalingen[index];
+          final ophaling = ophalingen[index];
           return GestureDetector(
             onTap: () {
               Navigator.push(
